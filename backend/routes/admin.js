0639ef6549d896d62e82a62648cce1d26d53f57a
Satyam204 <satyamshivam366@gmail.com>
2024-09-29 13:50:05 +0530

    email schema and box

--- backend/routes/admin.js
+++ backend/routes/admin.js
@@ -143,11 +143,11 @@ router.get('/admin/logout', (req, res) => {
 
 router.post('/register', async (req, res) => {
     try {
-      const { username, password } = req.body;
+      const { username, email,password } = req.body;
       const hashedPassword = await bcrypt.hash(password, 10);
   
       try {
-        const user = await User.create({ username, password:hashedPassword });
+        const user = await User.create({ username, email,password:hashedPassword });
         res.status(201).json({ message: 'User Created', user });
       } catch (error) {
         if(error.code === 11000) {
--- backend/server/models/User.js
+++ backend/server/models/User.js
@@ -5,11 +5,23 @@ const UserSchema = new Schema({
   username: {
     type: String,
     required: true,
-    unique:true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true // Ensure the email is unique
   },
   password: {
     type: String,
     required: true
+  },
+  verificationToken: {
+    type: String,
+    required: false // Optional until a user registers
+  },
+  isVerified: {
+    type: Boolean,
+    default: false // Set to false until verified
   }
 });
 
